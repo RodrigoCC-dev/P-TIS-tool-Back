c0ef89148a2c06f304be434d719f2895a24f1856
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-29 03:16:45 -0300

    BD: Se agrega validación en 'usuario' que permite registrar valores nulos en 'run'

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -12,10 +12,10 @@ class Usuario < ApplicationRecord
 
   # validaciones
   validates_uniqueness_of :email
-  validates :nombre, :apellido_paterno, :apellido_materno, :run, presence: true
+  validates :nombre, :apellido_paterno, :apellido_materno, presence: true
   validates :nombre, :apellido_paterno, :apellido_materno,
     format: {with: /\A[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+\z/, message: "Sólo se aceptan letras"}
-  validates :run, format: {with: /(\d{7,8})-(\d|K)/}
+  validates :run, format: {with: /(\d{7,8})-(\d|K)/}, presence: true, unless: :run_nil?
   validates :email,
     format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
     presence: true,
@@ -27,5 +27,9 @@ class Usuario < ApplicationRecord
     self.apellido_paterno = self.apellido_paterno.titleize
     self.apellido_materno = self.apellido_materno.titleize
     self.email = self.email.downcase
+  end
+
+  def run_nil?
+    self.run.nil?
   end
 end
