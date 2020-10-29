fc693c3c8668db2b7e544db3b24e3f237e04d2c7
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-29 20:29:54 -0300

    BD: Se agrega tabla intermedia entre 'items' y 'responsables' para relación muchos a muchos

--- db/migrate/20201019233922_create_responsables.rb
+++ db/migrate/20201019233922_create_responsables.rb
@@ -1,7 +1,6 @@
 class CreateResponsables < ActiveRecord::Migration[6.0]
   def change
     create_table :responsables do |t|
-      t.references :item, null: false, foreign_key: true
       t.references :asistencia, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_053914) do
+ActiveRecord::Schema.define(version: 2020_10_29_231604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,13 @@ ActiveRecord::Schema.define(version: 2020_10_29_053914) do
     t.index ["tipo_item_id"], name: "index_items_on_tipo_item_id"
   end
 
+  create_table "items_responsables", id: false, force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "responsable_id", null: false
+    t.index ["item_id"], name: "index_items_responsables_on_item_id"
+    t.index ["responsable_id"], name: "index_items_responsables_on_responsable_id"
+  end
+
   create_table "jornadas", force: :cascade do |t|
     t.string "nombre"
     t.integer "identificador"
@@ -201,12 +208,10 @@ ActiveRecord::Schema.define(version: 2020_10_29_053914) do
   end
 
   create_table "responsables", force: :cascade do |t|
-    t.bigint "item_id", null: false
     t.bigint "asistencia_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["asistencia_id"], name: "index_responsables_on_asistencia_id"
-    t.index ["item_id"], name: "index_responsables_on_item_id"
   end
 
   create_table "respuestas", force: :cascade do |t|
@@ -349,6 +354,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_053914) do
   add_foreign_key "estudiantes", "usuarios"
   add_foreign_key "items", "bitacora_revisiones"
   add_foreign_key "items", "tipo_items"
+  add_foreign_key "items_responsables", "items"
+  add_foreign_key "items_responsables", "responsables"
   add_foreign_key "minutas", "clasificaciones"
   add_foreign_key "minutas", "estudiantes"
   add_foreign_key "minutas", "tipo_minutas"
@@ -359,7 +366,6 @@ ActiveRecord::Schema.define(version: 2020_10_29_053914) do
   add_foreign_key "registros", "minutas"
   add_foreign_key "registros", "tipo_actividades"
   add_foreign_key "responsables", "asistencias"
-  add_foreign_key "responsables", "items"
   add_foreign_key "respuestas", "asistencias"
   add_foreign_key "respuestas", "comentarios"
   add_foreign_key "secciones", "cursos"
