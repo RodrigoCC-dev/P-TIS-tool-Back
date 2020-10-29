0745dcc3228b79114ecdd3207fde30f2e7f0a039
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-28 22:53:02 -0300

    Estudiantes: Se agrega controlador para agregar estudiantes al sistema

---
+++ app/controllers/estudiantes_controller.rb
@@ -0,0 +1,38 @@
+class EstudiantesController < ApplicationController
+  before_action :authenticate_usuario
+
+  def index
+  end
+
+  def create
+    estudiante = Estudiante.new
+    estudiante.build_usuario
+    estudiante.usuario.assign_attributes(estudiantes_params[:usuario_attributes])
+    estudiante.iniciales = obtener_iniciales(estudiante.usuario)
+    grupo_por_defecto = Grupo.find_by(nombre: 'SG')
+    estudiante.grupo_id = grupo_por_defecto.id
+    rol_estudiante = Rol.find_by(rol: 'Estudiante')
+    estudiante.rol_id = rol_estudiante.id
+    estudiante.assign_attributes(estudiantes_params)
+    estudiante.password = 'pass'
+    estudiante.password_confirmation = 'pass'
+    if estudiante.valid?
+      estudiante.save!
+    else
+      render json: ['error': 'Información del estudiante no es válida'], status: :unprocessable_entity
+    end
+  end
+
+  private
+  def estudiantes_params
+    params.require(:estudiante).permit(:seccion_id, usuario_attributes: [:nombre, :apellido_paterno, :apellido_materno, :run, :correo_elec])
+  end
+
+  def obtener_iniciales(usuario)
+    iniciales = ""
+    iniciales += usuario.nombre.chr.upcase
+    iniciales += usuario.apellido_paterno.chr.upcase
+    iniciales += usuario.apellido_materno.chr.upcase
+    return iniciales
+  end
+end
