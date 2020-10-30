8f7a604e0044b539f182098c34ed4b65616f0785
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-29 23:47:33 -0300

    Json format: Se agrega formato json como 'concerns' para mostrar información de 'Usuario'

--- app/controllers/concerns/json_format.rb
+++ app/controllers/concerns/json_format.rb
@@ -4,4 +4,8 @@ module JsonFormat
   def json_data
     { except: %i[created_at updated_at borrado deleted_at] }
   end
+
+  def user_data
+    {except: %i[created_at updated_at password_digest borrado deleted_at]}
+  end
 end
--- app/controllers/secciones_controller.rb
+++ app/controllers/secciones_controller.rb
@@ -4,7 +4,12 @@ class SeccionesController < ApplicationController
 
   def index
     semestreActual = Semestre.where('activo = ? AND borrado = ?', true, false).last
-    secciones = Seccion.where('semestre_id = ? AND borrado = ?', semestreActual.id, false)
+    usuario = Usuario.find(current_usuario.id)
+    if usuario.rol.rango == 1
+      secciones = Seccion.where('semestre_id = ? AND borrado = ?', semestreActual.id, false)
+    elsif usuario.rol.rango == 2
+      secciones = Seccion.joins(:profesores).where('profesores.usuario_id = ? AND semestre_id = ? AND borrado = ?', usuario.id, semestreActual.id, false)
+    end
     render json: secciones.as_json(
       {except: %i[jornada_id semestre_id curso_id borrado deleted_at created_at updated_at],
         :include => {
