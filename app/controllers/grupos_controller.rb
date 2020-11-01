787f54a0b69dd3629f3d02b42b13f22f7451e1eb
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-01 02:09:16 -0300

    Grupos: Se agrega servicio que entrega por jornada

--- app/controllers/grupos_controller.rb
+++ app/controllers/grupos_controller.rb
@@ -1,4 +1,18 @@
 class GruposController < ApplicationController
   before_action :authenticate_usuario
-  
+  include JsonFormat
+
+  def index
+  end
+
+  # Servicio que entrega los grupos de estudiantes disponibles asociados a una jornada
+  def por_jornada
+    grupo = Estudiante.joins(:grupo).joins(seccion: :jornada).where('grupos.borrado = ? AND grupos.nombre <> ? AND jornadas.nombre = ?', false, 'SG', params[:jornada]).select('
+      grupos.id,
+      grupos.nombre,
+      grupos.correlativo
+      ').last
+    render json: grupo.as_json(json_data)
+  end
+
 end
