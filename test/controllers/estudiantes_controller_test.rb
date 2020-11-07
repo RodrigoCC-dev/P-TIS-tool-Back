b188e3fc5be4326c4d25d38122da7d84de345e62
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-07 02:42:33 -0300

    Testing: Se realizan los test de acceso no autorizado en el controlador 'Estudiantes'

--- test/controllers/estudiantes_controller_test.rb
+++ test/controllers/estudiantes_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class EstudiantesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Revisión de acceso a rutas sin autenticación
+
+  test "Debería obtener código '401' al tratar de obtener 'index'" do
+    get estudiantes_url
+    assert_response 401
+  end
+
+  test "Debería obtener código '401' al tratar de postear 'create'" do
+    post '/estudiantes', params: {estudiante: {
+      seccion_id: secciones(:one),
+      usuario_attributes: {
+        nombre: 'Juan',
+        apellido_paterno: 'Castro',
+        apellido_materno: 'Mendez',
+        run: '12345678-9',
+        email: 'juan.castro@usach.cl',
+        }
+      }}
+    assert_response 401
+  end
+
+  test "Debería obtener código '401' al tratar de obtener 'sin_grupo'" do
+    get estudiantes_sin_grupo_url
+    assert_response 401
+  end
+
+  # Revisión del funcionamiento de index
+
 end
