e411b14fd728a4375227fd601d78320c7018e4cc
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-07 02:09:55 -0300

    Testing: Se realizan los test unitarios para los modelos 'Estudiante' y 'Grupo'

--- test/models/estudiante_test.rb
+++ test/models/estudiante_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class EstudianteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Estudiante sin iniciales no se guarda" do
+    estudiante = Estudiante.new(usuario: usuarios(:one),
+    grupo: grupos(:one), seccion: secciones(:one))
+    assert_not estudiante.save
+  end
+
+  test "Estudiante con iniciales se guarda" do
+    estudiante = Estudiante.new(iniciales: 'ABC',
+    usuario: usuarios(:two),
+    grupo: grupos(:two), seccion: secciones(:two))
+    assert estudiante.save
+  end
 end
