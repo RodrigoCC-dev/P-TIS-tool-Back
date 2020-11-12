72e75af9944dddd748229fbb69fae053bae9c784
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-12 18:15:43 -0300

    Testing: Se agregan test unitarios al modelo 'BitacoraRevision' para dato: 'emitida'

--- test/models/bitacora_revision_test.rb
+++ test/models/bitacora_revision_test.rb
@@ -10,9 +10,29 @@ class BitacoraRevisionTest < ActiveSupport::TestCase
     assert_not bitacora.save
   end
 
+  test "BitacoraRevision con 'emitida' igual a 'nil' no se guarda" do
+    bitacora = BitacoraRevision.new(
+      revision: 'B',
+      emitida: nil,
+      motivo_id: motivos(:one).id,
+      minuta_id: minutas(:one).id
+    )
+    assert_not bitacora.save
+  end
+
   test "BitacoraRevision con 'revision' con formato válido se guarda" do
     bitacora = BitacoraRevision.new(
       revision: 'C',
+      motivo_id: motivos(:one).id,
+      minuta_id: minutas(:one).id
+    )
+    assert bitacora.save
+  end
+
+  test "BitacoraRevision con 'revision' y 'emitida' con datos válidos se guarda" do
+    bitacora = BitacoraRevision.new(
+      revision: 'B',
+      emitida: true,
       motivo_id: motivos(:one).id,
       minuta_id: minutas(:one).id
     )
