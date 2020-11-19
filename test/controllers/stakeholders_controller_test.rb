fb4f0d514e8cfe026fea36a6aa1571ea8783c53e
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-19 02:27:26 -0300

    Testing: Se agregan test de integración para el servicio 'create' en el controlador 'Stakeholder'

--- test/controllers/stakeholders_controller_test.rb
+++ test/controllers/stakeholders_controller_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class StakeholdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Revisión del funcionamiento del servicio 'create'
+
+  test "Debería obtener código '401' al tratar de postear 'create'" do
+    post '/stakeholders', params: {stakeholder: {
+      grupo_id: grupos(:one).id,
+      usuario_attributes: {
+        nombre: 'Edgardo',
+        apellido_paterno: 'Venegas',
+        apellido_materno: 'Contreras',
+        email: 'edgardo.venegas@algo.com'
+      }
+    }}
+    assert_response 401
+  end
+
+  test "Debería poder crear un nuevo stakeholder como coodinador" do
+    assert_difference 'Stakeholder.count', 1 do
+      post '/stakeholders', params: {stakeholder: {
+        grupo_id: grupos(:one).id,
+        usuario_attributes: {
+          nombre: 'Edgardo',
+          apellido_paterno: 'Venegas',
+          apellido_materno: 'Contreras',
+          email: 'edgardo.venegas@algo.com'
+          }
+        }
+      }, headers: authenticated_header(usuarios(:coordinador), 'coordinacion')
+    end
+    assert_response :success
+  end
+
+  test "Debería poder crear un nuevo stakeholder como profesor" do
+    assert_difference 'Stakeholder.count', 1 do
+      post '/stakeholders', params: {stakeholder: {
+        grupo_id: grupos(:one).id,
+        usuario_attributes: {
+          nombre: 'Margarita',
+          apellido_paterno: 'Gonzalez',
+          apellido_materno: 'Soto',
+          email: 'margarita.gonzales@algo.com'
+          }
+        }
+      }, headers: authenticated_header(usuarios(:profesor), 'profe')
+    end
+    assert_response :success
+  end
+
 end
