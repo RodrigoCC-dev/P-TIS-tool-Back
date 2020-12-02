209cb05800321669f362ffc01bc36542414423f4
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-02 06:45:10 -0300

    NuevoProfesor: Se agrega servicio que entrega listado de profesores en el sistema

--- app/controllers/profesores_controller.rb
+++ app/controllers/profesores_controller.rb
@@ -3,5 +3,17 @@ class ProfesoresController < ApplicationController
   include JsonFormat
 
   # Servicio que entrega el listado de profesores en el sistema
-  
+  def index
+    profesores = Profesor.all
+    render json: profesores.as_json(
+      { except: %i[created_at updated_at], :include => {
+        :secciones => json_data,
+        :usuario => user_data
+        }
+      }
+    )
+  end
+
+  def create
+  end
 end
