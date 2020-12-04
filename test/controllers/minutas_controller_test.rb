14b8412d46b302cf913c27cd7a8827b47a0ae557
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-04 02:15:59 -0300

    Testing: Se agrega test integración para servicio 'update' en controlador 'Minuta'

--- test/controllers/minutas_controller_test.rb
+++ test/controllers/minutas_controller_test.rb
@@ -144,7 +144,142 @@ class MinutasControllerTest < ActionDispatch::IntegrationTest
   end
 
 
-  # Revisión de funcionamiento del servicio 'correlativo'
+  # Revisión del funcionamiento del servicio 'update'
+
+  test "Debería obtener código '401' al tratar de obtener 'update' sin autenticación" do
+    patch '/minutas/' + bitacora_revisiones(:one).id.to_s, params: {
+      id: bitacora_revisiones(:one).id,
+      minuta: {
+        estudiante_id: estudiantes(:Pablo).id,
+        correlativo: 1,
+        codigo: 'MINUTA_G01_05_2020-2_1112',
+        fecha_reunion: '2020-11-12',
+        h_inicio: '13:00',
+        h_termino: '14:00',
+        tipo_minuta_id: tipo_minutas(:one).id
+      },
+      clasificacion: {
+        informativa: false,
+        avance: false,
+        coordinacion: true,
+        decision: true,
+        otro: false
+      },
+      tema: 'Minuta de prueba',
+      objetivos: [
+        {id: 0, descripcion: 'Este es el primer objetivo'},
+        {id: 0, descripcion: 'Este es el segundo objetivo'}
+      ],
+      conclusiones: [
+        {id: 0, descripcion: 'Esta es la primera conclusión'},
+        {id: 0, descripcion: 'Esta es la segunda conclusion'}
+      ],
+      items: [{
+        correlativo: 1,
+        descripcion: 'Primer item',
+        fecha: '',
+        tipo_item_id: tipo_items(:one).id,
+        responsables: [{tipo: '', id: 0}]
+        },
+        {
+          correlativo: 2,
+          descripcion: 'Segundo item',
+          fecha: '2020-12-05',
+          tipo_item_id: tipo_items(:two).id,
+          responsables: [{tipo: 'est', id: estudiantes(:Pablo).id}]
+        }
+      ],
+      bitacora_revision: {
+        revision: 'A',
+        motivo_id: motivos(:one).id
+      },
+      asistencia: [
+        {estudiante: estudiantes(:Pablo).id, asistencia: tipo_asistencias(:one).id},
+        {estudiante: estudiantes(:two).id, asistencia: tipo_asistencias(:two).id}
+      ],
+      tipo_estado: tipo_estados(:one).id
+    }
+    assert_response 401
+  end
+
+  test "Debería poder actualizar la información de una minuta de reunión" do
+    @bitacora = bitacora_revisiones(:three)
+
+    patch '/minutas/' + bitacora_revisiones(:three).id.to_s, params: {
+      id: bitacora_revisiones(:three).id,
+      minuta: {
+        estudiante_id: estudiantes(:Pablo).id,
+        correlativo: 1,
+        codigo: 'MINUTA_G01_05_2020-2_1112',
+        fecha_reunion: '2020-11-12',
+        h_inicio: '13:00',
+        h_termino: '14:00',
+        tipo_minuta_id: tipo_minutas(:one).id
+      },
+      clasificacion: {
+        informativa: false,
+        avance: false,
+        coordinacion: true,
+        decision: true,
+        otro: false
+      },
+      tema: 'Minuta de prueba',
+      objetivos: [
+        {id: objetivos(:three).id, descripcion: 'Este es el primer objetivo'},
+        {id: objetivos(:four).id, descripcion: 'Este es el segundo objetivo'}
+      ],
+      conclusiones: [
+        {id: conclusiones(:three).id, descripcion: 'Esta es la primera conclusión'},
+        {id: conclusiones(:four).id, descripcion: 'Esta es la segunda conclusion'}
+      ],
+      items: [{
+        correlativo: 1,
+        descripcion: 'Primer item',
+        fecha: '',
+        tipo_item_id: tipo_items(:one).id,
+        responsables: [{tipo: '', id: 0}]
+        },
+        {
+          correlativo: 2,
+          descripcion: 'Segundo item',
+          fecha: '2020-12-05',
+          tipo_item_id: tipo_items(:two).id,
+          responsables: [{tipo: 'est', id: estudiantes(:Pablo).id}]
+        }
+      ],
+      bitacora_revision: {
+        revision: 'A',
+        motivo_id: motivos(:one).id
+      },
+      asistencia: [
+        {estudiante: estudiantes(:Pablo).id, asistencia: tipo_asistencias(:one).id},
+        {estudiante: estudiantes(:two).id, asistencia: tipo_asistencias(:two).id}
+      ],
+      tipo_estado: tipo_estados(:one).id
+    }, headers: authenticated_header(usuarios(:Pablo), 'pablo123')
+    @bitacora.reload
+    assert_equal @bitacora.minuta.codigo, 'MINUTA_G01_05_2020-2_1112'
+    assert_equal @bitacora.minuta.fecha_reunion, Date.new(2020,11,12)
+    assert_equal @bitacora.minuta.h_inicio, Time.utc(2000,1,1,13,0)
+    assert_equal @bitacora.minuta.h_termino, Time.utc(2000,1,1,14,0)
+    assert_equal @bitacora.minuta.clasificacion.informativa, false
+    assert_equal @bitacora.minuta.clasificacion.avance, false
+    assert_equal @bitacora.minuta.clasificacion.coordinacion, true
+    assert_equal @bitacora.minuta.clasificacion.decision, true
+    assert_equal @bitacora.minuta.clasificacion.otro, false
+    assert_equal @bitacora.tema.tema, 'Minuta de prueba'
+    assert_equal @bitacora.objetivos[0].descripcion, 'Este es el primer objetivo'
+    assert_equal @bitacora.objetivos[1].descripcion, 'Este es el segundo objetivo'
+    assert_equal @bitacora.conclusiones[0].descripcion, 'Esta es la primera conclusión'
+    assert_equal @bitacora.conclusiones[1].descripcion, 'Esta es la segunda conclusion'
+    assert_equal @bitacora.items[1].descripcion, 'Primer item'
+    assert_equal @bitacora.items[0].descripcion, 'Segundo item'
+    assert_equal @bitacora.items[0].fecha, Date.new(2020,12,5)
+    assert_response :success
+  end
+
+
+  # Revisión del funcionamiento del servicio 'correlativo'
 
   test "Debería obtener código '401' al tratar de obtener correlativo sin autenticación" do
     get '/minutas/correlativo/' + grupos(:one).id.to_s
