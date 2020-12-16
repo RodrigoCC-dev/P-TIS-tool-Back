db64ab996e6a3ad01a410aa55cd05af2e35cf275
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-16 03:17:37 -0300

    Testing: Se agregan test de integración a servicio 'update' del controlador 'Usuario'

--- test/controllers/usuarios_controller_test.rb
+++ test/controllers/usuarios_controller_test.rb
@@ -18,4 +18,51 @@ class UsuariosControllerTest < ActionDispatch::IntegrationTest
     get login_user_url, headers: authenticated_header(usuarios(:profesor), 'profe')
     assert_response :success
   end
+
+
+  # Revision del servicio update
+
+  test "Debería obtener código '401' al tratar de actualizar la clave de acceso sin autenticación" do
+    patch usuario_url(id: usuarios(:one).id), params: {
+      password: 'abc',
+      usuario: {
+        password: 'abc',
+        password_confirmation: 'abc'
+      }
+    }
+    assert_response 401
+  end
+
+  test "Debería obtener código '422' al tratar de actualizar clave de acceso con usuario distinto" do
+    patch usuario_url(id: usuarios(:two).id), params: {
+      password: 'MyString',
+      usuario: {
+        password: 'abc',
+        password_confirmation: 'abc'
+      }
+    }, headers: authenticated_header(usuarios(:one), 'MyString')
+    assert_response 422
+  end
+
+  test "Debería obtener código '422' al tratar de actualizar clave de acceso con clave actual erronea" do
+    patch usuario_url(id: usuarios(:one).id), params: {
+      password: 'Prueba',
+      usuario: {
+        password: 'abc',
+        password_confirmation: 'abc'
+      }
+    }, headers: authenticated_header(usuarios(:one), 'MyString')
+    assert_response 422
+  end
+
+  test "Debería poder actualizar la clave de acceso de mi usuario" do
+    patch usuario_url(id: usuarios(:one).id), params: {
+      password: 'MyString',
+      usuario: {
+        password: 'abc',
+        password_confirmation: 'abc'
+      }
+    }, headers: authenticated_header(usuarios(:one), 'MyString')
+    assert_response :success
+  end
 end
