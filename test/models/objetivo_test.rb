fa15b559c323956e57f3d449b2191e87194e2be0
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-25 18:00:55 -0300

    Testing: Se agregan test unitarios para comprobar validación del valor 'borrado' en modelos 'Conclusion', 'Objetivo' e 'Item'

--- test/models/objetivo_test.rb
+++ test/models/objetivo_test.rb
@@ -6,10 +6,37 @@ class ObjetivoTest < ActiveSupport::TestCase
     assert_not objetivo.save
   end
 
-  test "Objetivo con descripcion se guarda" do
+  test "Objetivo con 'descripcion' se guarda" do
     objetivo = Objetivo.new(
       descripcion: 'Este es un nuevo objetivo',
       bitacora_revision_id: bitacora_revisiones(:one).id
+    )
+    assert objetivo.save
+  end
+
+  test "Objetivo con 'borrado' en 'nil' no se guarda" do
+    objetivo = Objetivo.new(
+      descripcion: 'Este es un nuevo objetivo',
+      bitacora_revision_id: bitacora_revisiones(:one).id,
+      borrado: nil
+    )
+    assert_not objetivo.save
+  end
+
+  test "Objetivo con 'borrado' en 'false' se guarda" do
+    objetivo = Objetivo.new(
+      descripcion: 'Este es un nuevo objetivo',
+      bitacora_revision_id: bitacora_revisiones(:one).id,
+      borrado: false
+    )
+    assert objetivo.save
+  end
+
+  test "Objetivo con 'borrado' en 'true' se guarda" do
+    objetivo = Objetivo.new(
+      descripcion: 'Este es un nuevo objetivo',
+      bitacora_revision_id: bitacora_revisiones(:one).id,
+      borrado: true
     )
     assert objetivo.save
   end
