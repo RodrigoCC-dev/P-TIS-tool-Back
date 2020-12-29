1d25c2398b4f14098e6693b56d21ce1904b0529a
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-29 02:39:11 -0300

    Testing: Se agregan test de integración para nuevo servicio 'show' en controlador 'Comentarios'

--- test/controllers/comentarios_controller_test.rb
+++ test/controllers/comentarios_controller_test.rb
@@ -82,4 +82,31 @@ class ComentariosControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+
+  # Revisión del funcionamiento del servicio 'show'
+
+  test "Debería obtener código 401 al tratar de obtener 'show' sin autenticación" do
+    get comentario_url(id: bitacora_revisiones(:three).id)
+    assert_response 401
+  end
+
+  test "Debería poder obtener los comentarios de una minuta como estudiante" do
+    get comentario_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:Pablo), 'pablo123')
+    assert_response :success
+  end
+
+  test "Debería poder obtener los comentarios de una minuta como stakeholder" do
+    get comentario_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:stakeholder), 'cliente')
+    assert_response :success
+  end
+
+  test "Debería poder obtener los comentarios de una minuta como profesor" do
+    get comentario_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:profesor), 'profe')
+    assert_response :success
+  end
+
+  test "Debería poder obtener los comentarios de una minuta como coordinador" do
+    get comentario_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:coordinador), 'coordinacion')
+    assert_response :success
+  end
 end
