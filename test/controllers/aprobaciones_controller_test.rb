54e05a1f5a20926a0ecb98a0a93b21a301079f26
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-29 03:53:42 -0300

    Testing: Se agregan test de integración para servicio 'show' en controlador 'Aprobaciones'

--- test/controllers/aprobaciones_controller_test.rb
+++ test/controllers/aprobaciones_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class AprobacionesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #Revisión del funcionamiento del servicio 'show'
+
+  test "Debería obtener código 401 al tratar de obtener 'show' sin autenticación" do
+    get aprobacion_url(id: bitacora_revisiones(:three).id)
+    assert_response 401
+  end
+
+  test "Debería poder obtener 'show' como estudiante" do
+    get aprobacion_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:Pablo), 'pablo123')
+    assert_response :success
+  end
+
+  test "Debería poder obtener 'show' como profesor" do
+    get aprobacion_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:profesor), 'profe')
+    assert_response :success
+  end
+
+  test "Debería poder obtener 'show' como coordinador" do
+    get aprobacion_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:coordinador), 'coordinacion')
+    assert_response :success
+  end
+
+  test "Debería poder obtener 'show' como stakeholder" do
+    get aprobacion_url(id: bitacora_revisiones(:three).id), headers: authenticated_header(usuarios(:stakeholder), 'cliente')
+    assert_response :success
+  end
+
+
+  # Revisión del funcionamiento del servicio 'update'
+
+  test "Debería obtener código 401 al tratar de actualizar una aprobación sin autenticación" do
+    put aprobacion_url(id: bitacora_revisiones(:three).id)
+    assert_response 401
+  end
+
+  test "Debería poder actualizar una aprobación como estudiante" do
+    @aprobacion = aprobaciones(:Pablo)
+    put aprobacion_url(id: bitacora_revisiones(:three).id)
+    assert_response 401
+  end
 end
