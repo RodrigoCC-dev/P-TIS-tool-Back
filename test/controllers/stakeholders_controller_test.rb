bad643fdce60aa644dd5cb9501f32ab58517aecb
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-06 05:43:22 -0300

    Testing: Se corrigen test de integración para controlador 'Stakeholders' según nuevo modelo BDv1.3

--- test/controllers/stakeholders_controller_test.rb
+++ test/controllers/stakeholders_controller_test.rb
@@ -19,18 +19,29 @@ class StakeholdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "Debería poder obtener los stakeholders como estudiante" do
+    get stakeholders_url, headers: authenticated_header(usuarios(:Pablo), 'pablo123')
+    assert_response :success
+  end
+
+  test "Debería poder obtener los stakeholders como stakeholder" do
+    get stakeholders_url, headers: authenticated_header(usuarios(:stakeholder), 'cliente')
+    assert_response :success
+  end
 
   # Revisión del funcionamiento del servicio 'create'
 
   test "Debería obtener código '401' al tratar de postear 'create'" do
     post '/stakeholders', params: {stakeholder: {
-      grupo_id: grupos(:one).id,
       usuario_attributes: {
         nombre: 'Edgardo',
         apellido_paterno: 'Venegas',
         apellido_materno: 'Contreras',
         email: 'edgardo.venegas@algo.com'
-      }
+        }
+      },
+    grupo: {
+      id: grupos(:one).id
     }}
     assert_response 401
   end
@@ -38,13 +49,15 @@ class StakeholdersControllerTest < ActionDispatch::IntegrationTest
   test "Debería poder crear un nuevo stakeholder como coodinador" do
     assert_difference 'Stakeholder.count', 1 do
       post '/stakeholders', params: {stakeholder: {
-        grupo_id: grupos(:one).id,
         usuario_attributes: {
           nombre: 'Edgardo',
           apellido_paterno: 'Venegas',
           apellido_materno: 'Contreras',
           email: 'edgardo.venegas@algo.com'
           }
+        },
+        grupo: {
+          id: grupos(:one).id
         }
       }, headers: authenticated_header(usuarios(:coordinador), 'coordinacion')
     end
@@ -54,13 +67,15 @@ class StakeholdersControllerTest < ActionDispatch::IntegrationTest
   test "Debería poder crear un nuevo stakeholder como profesor" do
     assert_difference 'Stakeholder.count', 1 do
       post '/stakeholders', params: {stakeholder: {
-        grupo_id: grupos(:one).id,
         usuario_attributes: {
           nombre: 'Margarita',
           apellido_paterno: 'Gonzalez',
           apellido_materno: 'Soto',
           email: 'margarita.gonzales@algo.com'
           }
+        },
+        grupo: {
+          id: grupos(:one).id
         }
       }, headers: authenticated_header(usuarios(:profesor), 'profe')
     end
