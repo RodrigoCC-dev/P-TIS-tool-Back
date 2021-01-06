e4b1120bee606726d78f54c8cb587e01a25615e5
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-06 05:42:47 -0300

    Testing: Se corrigen test del modelo 'Stakeholder' según nuevo modelo de base de datos v1.3

--- test/models/stakeholder_test.rb
+++ test/models/stakeholder_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class StakeholderTest < ActiveSupport::TestCase
   test "Stakeholder sin 'iniciales' no se guarda" do
     stakeholder = Stakeholder.new(
-      usuario_id: usuarios(:one).id,
-      grupo_id: grupos(:one).id
+      usuario_id: usuarios(:one).id
     )
     assert_not stakeholder.save
   end
@@ -12,8 +11,7 @@ class StakeholderTest < ActiveSupport::TestCase
   test "Stakeholder con iniciales se guarda" do
     stakeholder = Stakeholder.new(
       iniciales: 'ABC',
-      usuario_id: usuarios(:one).id,
-      grupo_id: grupos(:one).id
+      usuario_id: usuarios(:one).id
     )
     assert stakeholder.save
   end
