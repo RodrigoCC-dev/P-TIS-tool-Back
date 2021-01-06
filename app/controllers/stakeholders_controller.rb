788ebeb710a55aaa194152eb4806f7ee7ae8f7bb
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-06 03:05:01 -0300

    AsignarStk: Se corrige servicio 'index' en controlador 'Stakeholders' según nuevo esquema BDv1.3

--- app/controllers/stakeholders_controller.rb
+++ app/controllers/stakeholders_controller.rb
@@ -7,11 +7,8 @@ class StakeholdersController < ApplicationController
   # Servicio que muestra los stakeholder ingresados en el sistema según las secciones asignadas al profesor
   def index
     if current_usuario.rol.rango == 1
-      stakeholders = Stakeholder.joins("INNER JOIN grupos_stakeholders ON grupos_stakeholders.stakeholder_id = stakeholders.id INNER JOIN grupos ON grupos.id = grupos_stakeholders.grupo_id
-        INNER JOIN estudiantes ON estudiantes.grupo_id = grupos.id INNER JOIN secciones ON secciones.id = estudiantes.seccion_id
-        INNER JOIN semestres ON semestres.id = secciones.semestre_id INNER JOIN usuarios ON usuarios.id = stakeholders.usuario_id
-        INNER JOIN jornadas ON jornadas.id = secciones.jornada_id").where('
-        semestres.id = ? AND usuarios.borrado = ?', @semestre_actual.id, false).select('
+      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
+        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ?', @semestre_actual.id, false).select('
         stakeholders.id,
         usuarios.nombre AS nombre_stk,
         usuarios.apellido_paterno AS apellido1,
@@ -21,12 +18,9 @@ class StakeholdersController < ApplicationController
         grupos.nombre AS nombre_grupo,
         jornadas.nombre AS jornada')
     elsif current_usuario.rol.rango == 2
-      stakeholders = Stakeholder.joins("INNER JOIN grupos_stakeholders ON grupos_stakeholders.stakeholder_id = stakeholders.id INNER JOIN grupos ON grupos.id = grupos_stakeholders.grupo_id
-        INNER JOIN estudiantes ON estudiantes.grupo_id = grupos.id INNER JOIN secciones ON secciones.id = estudiantes.seccion_id
-        INNER JOIN profesores_secciones ON profesores_secciones.seccion_id = secciones.id INNER JOIN profesores ON profesores.id = profesores_secciones.profesor_id
-        INNER JOIN semestres ON semestres.id = secciones.semestre_id INNER JOIN usuarios ON usuarios.id = stakeholders.usuario_id
-        INNER JOIN jornadas ON jornadas.id = secciones.jornada_id").where('
-        semestres.id = ? AND usuarios.borrado = ? AND profesores.usuario_id = ?', @semestre_actual.id, false, current_usuario.id).select('
+      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
+        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ? AND profesores.usuario_id = ?',
+        @semestre_actual.id, false, current_usuario.id).select('
         stakeholders.id,
         usuarios.nombre AS nombre_stk,
         usuarios.apellido_paterno AS apellido1,
@@ -38,17 +32,19 @@ class StakeholdersController < ApplicationController
     end
     lista = []
     stakeholders.each do |s|
-      unless presente_en_lista?(lista, s.id)
+      unless lista.include?(s)
         lista << s
       end
     end
     lista_final = []
     lista.each do |l|
+      aux = []
+      l.grupos.each do |g|
+        grupo = {id: g.id, nombre: g.nombre }
+        aux << grupo
+      end
       h = {id: l.id, nombre: l.nombre_stk, apellido_paterno: l.apellido1, apellido_materno: l.apellido2,
-        grupo: {
-          id: l.id_grupo,
-          nombre: l.nombre_grupo
-        },
+        grupos: aux,
         jornada: l.jornada
       }
       lista_final << h
