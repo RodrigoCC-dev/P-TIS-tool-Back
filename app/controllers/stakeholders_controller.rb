79990f4b591dd626d913320dff521dc384283393
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-06 03:52:30 -0300

    AsignarStk: Se cambia servicio 'index' por nuevo servicio 'por_jornadas' e 'index' se emplea para entregar stakeholders del sistema en controlaador 'Stakeholders'

--- app/controllers/stakeholders_controller.rb
+++ app/controllers/stakeholders_controller.rb
@@ -1,55 +1,18 @@
 class StakeholdersController < ApplicationController
   before_action :authenticate_usuario
-  before_action :semestre_actual, only: [:index]
+  before_action :semestre_actual, only: [:index, :por_jornada]
   include JsonFormat
   include Funciones
 
   # Servicio que muestra los stakeholder ingresados en el sistema según las secciones asignadas al profesor
   def index
-    if current_usuario.rol.rango == 1
-      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
-        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ?', @semestre_actual.id, false).select('
-        stakeholders.id,
-        usuarios.nombre AS nombre_stk,
-        usuarios.apellido_paterno AS apellido1,
-        usuarios.apellido_materno AS apellido2,
-        secciones.codigo AS codigo_seccion,
-        grupos.id AS id_grupo,
-        grupos.nombre AS nombre_grupo,
-        jornadas.nombre AS jornada')
-    elsif current_usuario.rol.rango == 2
-      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
-        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ? AND profesores.usuario_id = ?',
-        @semestre_actual.id, false, current_usuario.id).select('
-        stakeholders.id,
-        usuarios.nombre AS nombre_stk,
-        usuarios.apellido_paterno AS apellido1,
-        usuarios.apellido_materno AS apellido2,
-        secciones.codigo AS codigo_seccion,
-        grupos.id AS id_grupo,
-        grupos.nombre AS nombre_grupo,
-        jornadas.nombre AS jornada')
-    end
-    lista = []
-    stakeholders.each do |s|
-      unless lista.include?(s)
-        lista << s
-      end
-    end
-    lista_final = []
-    lista.each do |l|
-      aux = []
-      l.grupos.each do |g|
-        grupo = {id: g.id, nombre: g.nombre }
-        aux << grupo
-      end
-      h = {id: l.id, nombre: l.nombre_stk, apellido_paterno: l.apellido1, apellido_materno: l.apellido2,
-        grupos: aux,
-        jornada: l.jornada
+    stakeholders = Stakeholder.joins(:usuario).where('usuarios.borrado = ?', false)
+    render json: stakeholders.as_json(
+      { except: %i[created_at updated_at], :include => {
+        :usuario => user_data
+        }
       }
-      lista_final << h
-    end
-    render json: lista_final.as_json(json_data)
+    )
   end
 
   # Servicio que permite crear un nuevo stakeholder en el sistema
@@ -103,6 +66,55 @@ class StakeholdersController < ApplicationController
       render json: ['Error': 'No se han agregado stakeholders al grupo seleccionado'], status: :unprocessable_entity
     end
   end
+
+  # Servicio que muestra los stakeholder ingresados en el sistema según las secciones asignadas al profesor
+  def por_jornada
+    if current_usuario.rol.rango == 1
+      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
+        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ?', @semestre_actual.id, false).select('
+        stakeholders.id,
+        usuarios.nombre AS nombre_stk,
+        usuarios.apellido_paterno AS apellido1,
+        usuarios.apellido_materno AS apellido2,
+        secciones.codigo AS codigo_seccion,
+        grupos.id AS id_grupo,
+        grupos.nombre AS nombre_grupo,
+        jornadas.nombre AS jornada')
+    elsif current_usuario.rol.rango == 2
+      stakeholders = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).joins(:usuario).joins(
+        grupos: {estudiantes: [seccion: :semestre]}).where('semestres.id = ? AND usuarios.borrado = ? AND profesores.usuario_id = ?',
+        @semestre_actual.id, false, current_usuario.id).select('
+        stakeholders.id,
+        usuarios.nombre AS nombre_stk,
+        usuarios.apellido_paterno AS apellido1,
+        usuarios.apellido_materno AS apellido2,
+        secciones.codigo AS codigo_seccion,
+        grupos.id AS id_grupo,
+        grupos.nombre AS nombre_grupo,
+        jornadas.nombre AS jornada')
+    end
+    lista = []
+    stakeholders.each do |s|
+      unless lista.include?(s)
+        lista << s
+      end
+    end
+    lista_final = []
+    lista.each do |l|
+      aux = []
+      l.grupos.each do |g|
+        grupo = {id: g.id, nombre: g.nombre }
+        aux << grupo
+      end
+      h = {id: l.id, nombre: l.nombre_stk, apellido_paterno: l.apellido1, apellido_materno: l.apellido2,
+        grupos: aux,
+        jornada: l.jornada
+      }
+      lista_final << h
+    end
+    render json: lista_final.as_json(json_data)
+  end
+
 
   private
   def stakeholders_params
