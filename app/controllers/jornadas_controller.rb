1e58c447fe2dd89c21349d46aaadffa6719876f8
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-15 16:57:09 -0300

    Stakeholder: Se agrega información de jornadas de un stakeholder en servicio 'index' del controlador 'Jornadas'

--- app/controllers/jornadas_controller.rb
+++ app/controllers/jornadas_controller.rb
@@ -8,6 +8,8 @@ class JornadasController < ApplicationController
       jornadas = Jornada.where('borrado = ?', false)
     elsif current_usuario.rol.rango == 2
       jornadas = Profesor.joins(secciones: :jornada).where('profesores.usuario_id = ? AND jornadas.borrado = ?', current_usuario.id, false).select('jornadas.*')
+    elsif current_usuario.rol.rango == 4
+      jornadas = Stakeholder.joins(grupos: {estudiantes: [seccion: :jornada]}).where('stakeholders.usuario_id = ? AND jornadas.borrado = ?', current_usuario.id, false).select('jornadas.*')
     end
     render json: jornadas.as_json(json_data)
   end
