ad4657fbb8cc10cf9c6c46f44ead8d6512977c51
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-03-19 20:22:10 -0300

    Testing: Se agregan test unitarios de integración para servicio 'actividades_minutas' en controlador 'Registros'

--- test/controllers/registros_controller_test.rb
+++ test/controllers/registros_controller_test.rb
@@ -28,4 +28,32 @@ class RegistrosControllerTest < ActionDispatch::IntegrationTest
     get registro_url(id: bitacora_revisiones(:one).id), headers: authenticated_header(usuarios(:stakeholder), 'cliente')
     assert_response :success
   end
+
+
+  # Revisión del servicio 'actividades_minutas'
+
+  test "Debería obtener código 401 al tratar de obtener 'actividades_minutas' sin autenticación" do
+    get '/registros/grupo/' + grupos(:one).id.to_s
+    assert_response 401
+  end
+
+  test "Debería poder obtener 'actividades_minutas' como coordinador" do
+    get '/registros/grupo/' + grupos(:one).id.to_s, headers: authenticated_header(usuarios(:coordinador), 'coordinacion')
+    assert_response :success
+  end
+
+  test "Debería poder obtener 'actividades_minutas' profesor" do
+    get '/registros/grupo/' + grupos(:one).id.to_s, headers: authenticated_header(usuarios(:profesor), 'profe')
+    assert_response :success
+  end
+
+  test "Debería obtener código 422 al tratar de obtener 'actividades_minutas' como estudiante" do
+    get '/registros/grupo/' + grupos(:one).id.to_s, headers: authenticated_header(usuarios(:Pablo), 'pablo123')
+    assert_response 422
+  end
+
+  test "Debería obtener código 422 al tratar de obtener 'actividades_minutas' como stakeholder" do
+    get '/registros/grupo/' + grupos(:one).id.to_s, headers: authenticated_header(usuarios(:stakeholder), 'cliente')
+    assert_response 422
+  end
 end
