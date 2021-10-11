c1bdbc646084784405b2f78fc2e49194b49a810d
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-10-11 18:35:43 -0300

    Modelos: Se agregan validaciones de registro único para modelos 'motivo' y 'tipo_estado'

--- db/migrate/20201019213843_create_motivos.rb
+++ db/migrate/20201019213843_create_motivos.rb
@@ -2,7 +2,7 @@ class CreateMotivos < ActiveRecord::Migration[6.0]
   def change
     create_table :motivos do |t|
       t.string :motivo
-      t.string :identificador
+      t.string :identificador, unique: true
       t.boolean :borrado, default: false
       t.datetime :deleted_at
 
