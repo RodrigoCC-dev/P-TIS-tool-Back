53fd5564b852ff0d73a692f634d726bd35dd2753
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-10-11 18:19:27 -0300

    Modelos: Se agregan validaciones de registro único para modelo 'rol' y 'jornada'

--- db/migrate/20201019040518_create_jornadas.rb
+++ db/migrate/20201019040518_create_jornadas.rb
@@ -2,7 +2,7 @@ class CreateJornadas < ActiveRecord::Migration[6.0]
   def change
     create_table :jornadas do |t|
       t.string :nombre
-      t.integer :identificador
+      t.integer :identificador, unique: true
       t.boolean :borrado, default:false
       t.datetime :deleted_at
 
