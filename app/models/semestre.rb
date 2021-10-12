e87eaf7bed2f6d6ccd1990b66b3594cc73f5fd9f
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-10-11 21:56:44 -0300

    Modelos: Se agregan validaciones de registro único para modelo 'semestre'

--- app/models/semestre.rb
+++ app/models/semestre.rb
@@ -1,3 +1,13 @@
 class Semestre < ApplicationRecord
   has_many :secciones
+  before_create :crear_identificador
+
+  # Validaciones
+  validates :identificador, uniqueness: true
+  validates :identificador, format: {with: /(\d{1})-(\d{4})/}
+
+  private
+  def crear_identificador
+    self.identificador = self.numero + '-' + self.agno
+  end
 end
